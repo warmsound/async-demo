8292e21640e34a8ccde5bbc5dd20448ad5858fc5
Vince Wong <vince@ixara.com>
2018-07-30 18:28:45 +0100

    Test delayed responses; minimum loader time test

--- test/test.js
+++ test/test.js
@@ -1,35 +1,72 @@
 describe('StoryViewer', () => {
 	var expect = chai.expect;
+
+	// Fake XMLHttpRequest spies.
 	var openSpy;
 	var sendAsyncSpy;
 
 	var viewer;
 
+	// StoryViewer spies.
+	var setLoaderVisibleSpy;
+	var setStoryTitleSpy;
+	var insertChapterSpy;
+
+	/** 
+	 * Return a Promise that resolves with the specified value, after the specified timeout.
+	 * @param {Any} resolveWithValue Value with which to resolve Promise.
+	 * @param {Integer} timeout Number of milliseconds after which to resolve Promise
+	 * @returns {Promise}
+	*/
+	function delay(resolveWithValue, timeout) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve(resolveWithValue);
+			}, timeout);
+		});
+	};
+
 	before(() => {
 		XHRMock.setup();
+
 		openSpy = sinon.spy(XMLHttpRequest.prototype, 'open');
 		sendAsyncSpy = sinon.spy(XMLHttpRequest.prototype, 'sendAsync');
+
+		setLoaderVisibleSpy = sinon.spy(StoryViewer.prototype, 'setLoaderVisible');
+		setStoryTitleSpy = sinon.spy(StoryViewer.prototype, 'setStoryTitle');
+		insertChapterSpy = sinon.spy(StoryViewer.prototype, 'insertChapter');
 	});
 
 	afterEach(() => {
 		XHRMock.reset(); // Reset request handlers.
+
 		openSpy.resetHistory();
-		sendAsyncSpy.resetHistory();		
+		sendAsyncSpy.resetHistory();
+
+		setLoaderVisibleSpy.resetHistory();
+		setStoryTitleSpy.resetHistory();
+		insertChapterSpy.resetHistory();
+
 		viewer.reset();
 	});
 
 	after(() => {
 		XHRMock.teardown();
+
 		openSpy.restore();
-		sendAsyncSpy.restore();		
+		sendAsyncSpy.restore();	
+		
+		setLoaderVisibleSpy.restore();
+		setStoryTitleSpy.restore();
+		insertChapterSpy.restore();
 	});
 
 	it('Should immediately show loader', () => {
 		var loader = document.getElementById('loader');
 
-		expect(getComputedStyle(loader).visibility).to.equal('hidden');
+		expect(setLoaderVisibleSpy.called).to.be.false;
 		viewer = new StoryViewer();
-		expect(getComputedStyle(loader).visibility).to.equal('visible');
+		expect(setLoaderVisibleSpy.calledWith(true)).to.be.true;
 	});
 
 	it('Should immediately request story', () => {
@@ -39,7 +76,43 @@ describe('StoryViewer', () => {
 		expect(sendAsyncSpy.called).to.be.true;
 	});
 
-	it('Should hide loader immediately when story is received', () => {
+	it('Should hide loader immediately when story is received (response delayed by 2s)', () => {
+		XHRMock.get('story', (req, res) => {
+			var response = res.status(200).body(JSON.stringify({
+				title: 'My Story',
+				chapters: []
+			}));
+
+			return delay(response, 2000);
+		});
+
+		viewer = new StoryViewer();
+
+		// sendAsyncSpy() returns a Promise that resolves when the response has been sent.
+		// Test is done() once Promise returned by then() resolves.
+		return sendAsyncSpy.returnValues[0].then(() => {
+			expect(setLoaderVisibleSpy.calledWith(false)).to.be.true;
+		});
+	}).timeout(5000);
+
+	it('Should set story title immediately when story is received (response delayed by 2s)', () => {
+		XHRMock.get('story', (req, res) => {
+			var response = res.status(200).body(JSON.stringify({
+				title: 'My Story',
+				chapters: []
+			}));
+
+			return delay(response, 2000);
+		});
+
+		viewer = new StoryViewer();
+
+		return sendAsyncSpy.returnValues[0].then(() => {
+			expect(setStoryTitleSpy.calledWith('My Story')).to.be.true;
+		});
+	}).timeout(5000);
+
+	it('Should show loader for at least 1 second', done => {
 		XHRMock.get('story', (req, res) => {
 			return res.status(200).body(JSON.stringify({
 				title: 'My Story',
@@ -49,10 +122,18 @@ describe('StoryViewer', () => {
 
 		viewer = new StoryViewer();
 
-		// sendAsyncSpy() returns a Promise that resolves when the response has been sent.
-		// Test is done() once Promise returned by then() resolves.
-		return sendAsyncSpy.returnValues[0].then(() => {
-			expect(getComputedStyle(loader).visibility).to.equal('hidden');
+		// Loader should not be hidden immediately.
+		expect(setLoaderVisibleSpy.calledWith(false)).to.be.false;
+
+		// Loader should not be hidden immediately after receiving story.
+		sendAsyncSpy.returnValues[0].then(() => {
+			expect(setLoaderVisibleSpy.calledWith(false)).to.be.false;
 		});
-	});
+
+		// Loader should be hidden after 1s (check after 2s).
+		setTimeout(() => {
+			expect(setLoaderVisibleSpy.calledWith(false)).to.be.true;
+			done();
+		}, 2000);
+	}).timeout(5000);
 });
